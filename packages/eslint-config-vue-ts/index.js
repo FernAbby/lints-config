f09e56ea9b3c40c91c514438024571cc8680353e
文珍 <wenzhen@yuanxinjituan.com>
2024-03-14 18:40:23 +0800

    feat:  新增@lints-config/eslint-config-vue-ts校验文件

--- packages/eslint-config-vue-ts/index.js
+++ packages/eslint-config-vue-ts/index.js
@@ -9,75 +9,18 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
-  plugins: ['import'],
+  plugins: ['vue', 'import'],
   parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: '@typescript-eslint/parser',
-    ecmaVersion: 2020,
+    ecmaVersion: 2021,
     sourceType: 'module',
-    jsxPragma: 'React',
     ecmaFeatures: {
       jsx: true,
     },
   },
+  ignorePatterns: ['dist/', 'build/', 'node_modules/', '**/*.min.js', '**/*.md', '**/*.css'],
   rules: {
-    // import
+    // eslint-plugin-import 导入顺序
     'import/order': ['error', { groups: ['builtin', 'external', 'internal'] }],
-    // Vue
-    'vue/script-setup-uses-vars': 'error',
-    // Typescript
-    '@typescript-eslint/ban-ts-ignore': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    'vue/custom-event-name-casing': 'off',
-    'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    // '@typescript-eslint/no-unused-vars': [
-    //   'error',
-    //   {
-    //     argsIgnorePattern: '^_',
-    //     varsIgnorePattern: '^_',
-    //   },
-    // ],
-    '@typescript-eslint/no-unused-vars': 'off',
-    'no-unused-vars': 'off',
-    // 'no-unused-vars': [
-    //   'error',
-    //   {
-    //     argsIgnorePattern: '^_',
-    //     varsIgnorePattern: '^_',
-    //   },
-    // ],
-    'space-before-function-paren': 'off',
-
-    'vue/attributes-order': 'off',
-    'vue/one-component-per-file': 'off',
-    'vue/html-closing-bracket-newline': 'off',
-    'vue/max-attributes-per-line': 'off',
-    'vue/multiline-html-element-content-newline': 'off',
-    'vue/singleline-html-element-content-newline': 'off',
-    'vue/attribute-hyphenation': 'off',
-    'vue/require-default-prop': 'off',
-    'vue/require-explicit-emits': 'off',
-    'vue/html-self-closing': [
-      'error',
-      {
-        html: {
-          void: 'always',
-          normal: 'never',
-          component: 'always',
-        },
-        svg: 'always',
-        math: 'always',
-      },
-    ],
-    'vue/multi-word-component-names': 'off',
-    'vue/no-v-html': 'off',
   },
 };
